41ea71ce95a159869d02c542236fcbf7021c9bc2
RetroDreams <spdeehring@gmail.com>
2022-07-18 04:06:42 -0500

    Clean up, logging, catching exceptions

--- game_role.js
+++ game_role.js
@@ -9,32 +9,32 @@ logger.remove(logger.transports.Console)
 logger.add(new logger.transports.Console, { colorize: true })
 logger.level = 'debug'
 
-const BOT_TOKEN = process.env.BOT_TOKEN
-
-client.login(BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function (evt) {
   logger.info(`Logged in as: ${client.user.tag}`)
 })
 
-const PLAYING_ROLE_ID = "998484966373605397" // our server specific role ID
-
 client.on('presenceUpdate', (oldMember, newMember) => {
-  const guild = newMember.guild;
-  const playingRole = guild.roles.cache.get(PLAYING_ROLE_ID);
+  try {
+    const playingRole = newMember.guild.roles.cache.get("998484966373605397"); // our server specific role ID
 
-  if (newMember.user.bot || newMember.clientStatus === 'mobile' || oldMember.status !== newMember.status) return;
+    if (newMember.user.bot || newMember.clientStatus === 'mobile' || oldMember.status !== newMember.status) return; // ignore these types of events
 
-  const oldGame = oldMember.activities.find(activity => activity.type === 1) ? true : false; // activity type 1 is 'playing a game'
-  const newGame = newMember.activities.find(activity => activity.type === 1) ? true : false;
+    const oldGame = oldMember.activities.find(activity => activity.type === 0) ? true : false; // activity type 0 is 'playing a game'
+    const newGame = newMember.activities.find(activity => activity.type === 0) ? true : false;
 
-  if (!oldGame && newGame) {         // started playing
-    newMember.member.roles.add(playingRole)
-      .then(() => console.log(`${playingRole.name} added to ${newMember.user.tag}`))
-      .catch(console.error);
-  } else if (oldGame && !newGame) {  // stopped playing
-    newMember.member.roles.remove(playingRole)
-      .then(() => console.log(`${playingRole.name} removed from ${newMember.user.tag}`))
-      .catch(console.error);
+    if (!oldGame && newGame) {         // started playing
+      newMember.member.roles.add(playingRole)
+        .then(() => logger.info(`${playingRole.name} added to ${newMember.user.tag}`))
+        .catch(logger.error);
+    } else if (oldGame && !newGame) {  // stopped playing
+      newMember.member.roles.remove(playingRole)
+        .then(() => logger.info(`${playingRole.name} removed from ${newMember.user.tag}`))
+        .catch(logger.error);
+    }
+  } catch (error) {
+    logger.error(error);
+    return;
   }
 });
