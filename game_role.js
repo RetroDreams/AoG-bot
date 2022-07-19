61faa99f7161c3835dd305606c2172e4ed748734
RetroDreams <spdeehring@gmail.com>
2022-07-19 03:07:09 -0500

    Add live streaming presence detection

--- game_role.js
+++ game_role.js
@@ -14,29 +14,45 @@ client.on('ready', function (evt) {
 
 client.on('presenceUpdate', (oldMember, newMember) => {
   try {
-    const playingRole = newMember.guild.roles.cache.get("998484966373605397") // our server specific role ID
+    const playingRole = newMember.guild.roles.cache.get('998484966373605397')
+    const streamingRole = newMember.guild.roles.cache.get('841771818737729567')
 
     const oldGame = oldMember.activities.find(activity => activity.type === 0) // activity type 0 is 'playing a game'
     const newGame = newMember.activities.find(activity => activity.type === 0)
+
+    const oldLivestream = oldMember.activities.find(activity => activity.type === 1) // activity type 1 is 'streaming a game'
+    const newLivestream = newMember.activities.find(activity => activity.type === 1)
 
     // ignore these types of events
     if (newMember.user.bot||
     newMember.clientStatus === 'mobile' ||
     oldMember.status !== newMember.status) return
-
+    
+    // playing game
     if (oldGame === undefined && newGame !== undefined) { // started playing
       // ignore these games
       if (['Wallpaper Engine', 'About Me'].includes(newGame.name)) {
-        logger.debug(`ignore list: ${newMember.user.tag} - ${newGame.name}`)
+        logger.debug(`playing ignore list: ${newMember.user.tag} - ${newGame.name}`)
         return
       }
       newMember.member.roles.add(playingRole)
-        .then(() => logger.info(`added: ${newMember.user.tag} - ${newGame.name}`))
+        .then(() => logger.info(`playing added: ${newMember.user.tag} - ${newGame.name}`))
         .catch(logger.error)
-    
     } else if (oldGame !== undefined && newGame === undefined) { // stopped playing
       newMember.member.roles.remove(playingRole)
-        .then(() => logger.info(`removed: ${newMember.user.tag} - ${oldGame.name}`))
+        .then(() => logger.info(`playing removed: ${newMember.user.tag} - ${oldGame.name}`))
+        .catch(logger.error)
+    }
+
+    // live streaming
+    if (oldLivestream === undefined && newLivestream !== undefined) { // started streaming
+      newMember.member.roles.add(streamingRole)
+        .then(() => logger.info(`streaming added: ${newMember.user.tag} - ${newLivestream.name}`))
+        .catch(logger.error)
+
+    } else if (oldLivestream !== undefined && newLivestream === undefined) { // stopped streaming
+      newMember.member.roles.remove(streamingRole)
+        .then(() => logger.info(`streaming removed: ${newMember.user.tag} - ${oldLivestream.name}`))
         .catch(logger.error)
     }
   } catch (error) {
