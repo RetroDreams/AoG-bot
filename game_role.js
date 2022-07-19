15c0a8a7952cdc6464e4b47e911a063d1f4d0f87
RetroDreams <spdeehring@gmail.com>
2022-07-18 19:28:55 -0500

    Logic improvements

--- game_role.js
+++ game_role.js
@@ -1,40 +1,46 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences] });
-
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
 const logger = require('winston')
 
 logger.remove(logger.transports.Console)
 logger.add(new logger.transports.Console, { colorize: true })
 logger.level = 'debug'
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences] })
 client.login(process.env.BOT_TOKEN)
-
 client.on('ready', function (evt) {
   logger.info(`Logged in as: ${client.user.tag}`)
 })
 
 client.on('presenceUpdate', (oldMember, newMember) => {
   try {
-    const playingRole = newMember.guild.roles.cache.get("998484966373605397"); // our server specific role ID
+    const playingRole = newMember.guild.roles.cache.get("998484966373605397") // our server specific role ID
 
-    if (newMember.user.bot || newMember.clientStatus === 'mobile' || oldMember.status !== newMember.status) return; // ignore these types of events
+    const oldGame = oldMember.activities.find(activity => activity.type === 0) // activity type 0 is 'playing a game'
+    const newGame = newMember.activities.find(activity => activity.type === 0)
 
-    const oldGame = oldMember.activities.find(activity => activity.type === 0) ? true : false; // activity type 0 is 'playing a game'
-    const newGame = newMember.activities.find(activity => activity.type === 0) ? true : false;
+    // ignore these types of events
+    if (newMember.user.bot||
+    newMember.clientStatus === 'mobile' ||
+    oldMember.status !== newMember.status) return
 
-    if (!oldGame && newGame) {         // started playing
+    if (oldGame === undefined && newGame !== undefined) { // started playing
+      // ignore these games
+      if (['Wallpaper Engine'].includes(newGame.name)) {
+        logger.debug(`ignore list: ${newMember.user.tag} - ${newGame.name}`)
+        return
+      }
       newMember.member.roles.add(playingRole)
-        .then(() => logger.info(`${playingRole.name} added to ${newMember.user.tag}`))
-        .catch(logger.error);
-    } else if (oldGame && !newGame) {  // stopped playing
+        .then(() => logger.info(`added: ${newMember.user.tag} - ${newGame.name}`))
+        .catch(logger.error)
+    
+    } else if (oldGame !== undefined && newGame === undefined) { // stopped playing
       newMember.member.roles.remove(playingRole)
-        .then(() => logger.info(`${playingRole.name} removed from ${newMember.user.tag}`))
-        .catch(logger.error);
+        .then(() => logger.info(`removed: ${newMember.user.tag} - ${oldGame.name}`))
+        .catch(logger.error)
     }
   } catch (error) {
-    logger.error(error);
-    return;
+    logger.error(error)
+    return
   }
-});
+})
